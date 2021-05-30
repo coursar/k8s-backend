f5f3ca82897926de5fad6b9ea8235d9c8938a2cf
student <student@localhost>
2021-05-30 13:04:11 +0300

    feat: uuid write

--- src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
+++ src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -15,7 +20,8 @@ public class K8sBackendApplication {
   private final String id = UUID.randomUUID().toString();
 
   @GetMapping
-  public String hello() {
+  public String hello() throws IOException {
+    Files.write(Path.of("/data/" + id + ".txt"), id.getBytes(StandardCharsets.UTF_8));
     return id;
   }
 
