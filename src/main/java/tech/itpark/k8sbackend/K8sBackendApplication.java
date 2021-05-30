5b7477e99d08c8fad9827c05c746606769059bc4
student <student@localhost>
2021-05-30 12:52:23 +0300

    feat: uuid

--- src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
+++ src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
@@ -6,14 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/**")
 public class K8sBackendApplication {
+  private final String id = UUID.randomUUID().toString();
 
   @GetMapping
   public String hello() {
-    return "ok";
+    return id;
   }
 
   public static void main(String[] args) {
