8fb02b42694b80cfac815ede3069ceb7eb4d3d74
coursar <coursar@gmail.com>
2021-06-01 19:47:22 +0300

    fix stuff

--- src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
+++ src/main/java/tech/itpark/k8sbackend/K8sBackendApplication.java
@@ -2,17 +2,14 @@ package tech.itpark.k8sbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.UUID;
 
+@CrossOrigin
 @SpringBootApplication
 @RestController
 @RequestMapping("/**")
@@ -20,8 +17,9 @@ public class K8sBackendApplication {
   private final String id = UUID.randomUUID().toString();
 
   @GetMapping
-  public String hello() throws IOException {
-    Files.write(Path.of("/data/" + id + ".txt"), id.getBytes(StandardCharsets.UTF_8));
+  public String hello() {
+    // uncomment for stateful app
+    // Files.write(Path.of("/data/" + id + ".txt"), id.getBytes(StandardCharsets.UTF_8));
     return id;
   }
 
